72e6528b3685cad24b38a167fa8474ab6b7b320e
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-04-07 12:03:18 +0200

    Add ACF example cod

--- public/app/themes/wp-starter/lib/ACF.php
+++ public/app/themes/wp-starter/lib/ACF.php
@@ -4,7 +4,27 @@ namespace WPStarter;
 
 class ACF {
   public static function register() {
-    // Register ACF fields
+    acf_add_local_field_group(array(
+      'key' => 'extra',
+      'title' => 'Extra',
+      'fields' => array (
+        array (
+          'key' => 'extra_field_1',
+          'label' => 'Link',
+          'name' => 'link',
+          'type' => 'url',
+        ),
+      ),
+      'location' => array (
+        array (
+          array (
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => 'post',
+          ),
+        ),
+      ),
+    ));
   }
 }
 
